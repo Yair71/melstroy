25562a23fec6f59ca84a62e19af0bda3bc829a3e
Yair71 <130748808+Yair71@users.noreply.github.com>
2026-03-13 23:44:56 +0200

    Update camera.js

--- games/murino-run/camera.js
+++ games/murino-run/camera.js
@@ -13,7 +13,6 @@ export function initCamera(scene) {
     return camera;
 }
 
-// МГНОВЕННЫЙ СБРОС (фиксит баг, когда при рестарте видно место смерти)
 export function resetCamera() {
     if (camera) {
         camera.position.set(0, 5, 10);
@@ -28,9 +27,13 @@ export function updateCamera(playerGroup, fogGroup, deltaTime) {
     if (gameState.current === STATE.INTRO || gameState.current === STATE.PLAYING) {
         deathTimer = 0;
         camera.position.x += (playerGroup.position.x - camera.position.x) * 0.1;
-        camera.position.z = playerGroup.position.z + 8;
-        camera.position.y = playerGroup.position.y + 4;
-        camera.lookAt(playerGroup.position.x, playerGroup.position.y + 2, playerGroup.position.z - 5);
+        
+        // --- НОВЫЕ КООРДИНАТЫ КАМЕРЫ (ВЫШЕ И ДАЛЬШЕ) ---
+        camera.position.z = playerGroup.position.z + 12; // Было 8
+        camera.position.y = playerGroup.position.y + 8;  // Было 4
+        
+        // Смотрим чуть сверху вниз на дорогу перед игроком
+        camera.lookAt(playerGroup.position.x, playerGroup.position.y + 3, playerGroup.position.z - 5);
     }
     else if (gameState.current === STATE.DYING) {
         deathTimer += deltaTime;
@@ -39,23 +42,16 @@ export function updateCamera(playerGroup, fogGroup, deltaTime) {
         const headPos = new THREE.Vector3(playerGroup.position.x, playerGroup.position.y + 3.5, playerGroup.position.z);
 
         if (deathTimer < 1.2) {
-            // ФАЗА 1: Смотрим со стороны, как он падает (1.2 секунды)
             camera.lookAt(playerGroup.position);
         } 
         else if (deathTimer < 1.7) {
-            // ФАЗА 2: Переключаемся в глаза и смотрим вперед на препятствие
             camera.position.copy(headPos);
             camera.lookAt(headPos.x, headPos.y, headPos.z - 10);
         } 
         else {
-            // ФАЗА 3: Плавный поворот шеи назад на Фога
             camera.position.copy(headPos);
-            
-            // Направляем невидимую камеру прямо на лицо Фога
             dummyCam.position.copy(camera.position);
             dummyCam.lookAt(fogGroup.position.x, fogGroup.position.y + 3, fogGroup.position.z);
-            
-            // Плавно интерполируем (поворачиваем) основную камеру к невидимой
             camera.quaternion.slerp(dummyCam.quaternion, 0.08); 
         }
     }
