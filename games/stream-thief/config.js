cec283d694fb78f9ea2cc0769c8b2ec614df475d
Yair71 <130748808+Yair71@users.noreply.github.com>
2026-03-18 20:35:55 +0200

    Update config.js

--- games/stream-thief/config.js
+++ games/stream-thief/config.js
@@ -1,11 +1,11 @@
-// games/stream-thief/config.js
+
 
 export const ASSETS = {
   models: {
     hand: './assets/hand.glb',
     room: './assets/room.glb',
     items: './assets/items.glb',
-    // Твои сидячие модельки из скриншота:
+  
     sit2: './assets/sit2.glb',
     sit3: './assets/sit3.glb',
     sitwait: './assets/sitwait.glb',
@@ -18,11 +18,11 @@ export const ASSETS = {
 export const CONFIG = {
   handExtendSpeed: 8.0,
   handRetractSpeed: 20.0,
-  handBaseZ: 8,       // Рука стартует за камерой
+  handBaseZ: 8,       
   
-  streamerHeight: 4.5, // Желаемый размер Мела
-  streamerY: 1.5,      // Высота, чтобы попа ровно сидела на стуле (подгонишь под свой room.glb)
-  streamerZ: 2.0       // Позиция Мела (между камерой и столом)
+  streamerHeight: 4.5, 
+  streamerY: 1.5,     
+  streamerZ: 2.0       
 };
 
 export const STATE = {
