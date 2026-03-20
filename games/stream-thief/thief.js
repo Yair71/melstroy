314e0f90e06f115b6204ed50ba792e642abc7b42
Yair71 <130748808+Yair71@users.noreply.github.com>
2026-03-20 18:02:02 +0200

    Update thief.js

--- games/stream-thief/thief.js
+++ games/stream-thief/thief.js
@@ -9,23 +9,24 @@ export function initThief(scene) {
   
   const handGltf = loadedAssets.models['hand'];
   if (handGltf) {
-    // Увеличиваем/уменьшаем руку, если нужно
     handGltf.scene.scale.set(CONFIG.handScale, CONFIG.handScale, CONFIG.handScale);
     
-    // Центрируем модельку руки внутри группы (на всякий случай)
+    // Центрируем модельку руки внутри группы
     const box = new THREE.Box3().setFromObject(handGltf.scene);
     const center = box.getCenter(new THREE.Vector3());
     handGltf.scene.position.set(-center.x, -center.y, -center.z);
 
+    // Поворачиваем руку так, чтобы она тянулась ОТ камеры к столу
+    handGltf.scene.rotation.y = Math.PI; 
+
     handGroup.add(handGltf.scene);
   }
 
-  // Стартовая позиция. Z=6.5 - это ПЕРЕД камерой (камера на Z=8)
-  // Теперь ты точно должен ее увидеть!
-  handGroup.position.set(0, CONFIG.handBaseY, CONFIG.handBaseZ);
+  // Ставим руку справа-снизу от камеры
+  handGroup.position.set(CONFIG.handBaseX, CONFIG.handBaseY, CONFIG.handBaseZ);
   scene.add(handGroup);
 }
 
 export function updateThief(deltaTime) {
-  // Пока рука просто висит, логику движения подключим позже
+  // Логику движения руки добавим, как только убедимся, что визуал идеальный
 }
