686e7650b74257308c900f38a802fe67c7423540
Yair71 <130748808+Yair71@users.noreply.github.com>
2026-03-20 19:04:23 +0200

    Update loot.js

--- games/stream-thief/loot.js
+++ games/stream-thief/loot.js
@@ -1,18 +1,1 @@
-// games/stream-thief/loot.js
-import { loadedAssets } from './assets.js';
-import { CONFIG } from './config.js';
-import { tableTopY, tableCenterZ } from './world.js'; // Берем координаты стола
 
-export function initLoot(scene) {
-  const itemsGltf = loadedAssets.models['items'];
-  if (itemsGltf) {
-    // Подгоняем размер лута
-    itemsGltf.scene.scale.set(CONFIG.lootScale, CONFIG.lootScale, CONFIG.lootScale);
-    
-    // Ставим ровно на поверхность стола, который мы нашли в world.js
-    // Если items.glb имеет центр внизу, он ляжет идеально.
-    itemsGltf.scene.position.set(0, tableTopY, tableCenterZ + 1); 
-    
-    scene.add(itemsGltf.scene);
-  }
-}
